7e49dfc907c0e1e54dd25a92514caf6d5b1f351f
Amir Jaffari <amir.jaffari@shopify.com>
2019-03-12 15:42:14 -0400

    Added js update

--- script/script.js
+++ script/script.js
@@ -7,25 +7,7 @@ $(function(){
 
 //Random Hex Generator
 
-let getColor = function(){
 
-	'#' + (function co(lor){  
-	 return (lor +=
-  [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)])
-  && (lor.length == 6) ?  lor : co(lor); })('');
-}
-
-
-let changeBackground = (function() {
-	$('header').css('background',getColor());
-});
-
-
-$('header').on('click',function(){
-
-	$('header').css('background',getColor());
-	console.log('2')
-});
 
 //Declare totals
 	let $priceTotal = 0;
